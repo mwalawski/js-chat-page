14a94183d46747491e26edefd00f05cbc798cd64
mwalawski <michal.walawski@awesome-ind.com>
2023-04-21 01:04:58 +0200

    fix deployment

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# git push -f git@github.com:mwalawski/js-chat-page.git main:gh-pages
+git push -f git@github.com:mwalawski/js-chat-page.git main:gh-pages
 
 cd -
